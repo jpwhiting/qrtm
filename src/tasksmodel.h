b4487fb9ebe9c828b4fb0f47e325305816dbf009
Jeremy Whiting <jpwhiting@kde.org>
2012-07-17 19:12:01 -0600

    Make TasksModel addTask and clear public.

--- src/tasksmodel.h
+++ src/tasksmodel.h
@@ -43,21 +43,20 @@ public:
     };
 
     explicit TasksModel(QObject *parent = 0);
-    
+
     virtual int rowCount(const QModelIndex &index) const;
     virtual QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
     virtual QVariant data ( const QModelIndex & index, int role = Qt::DisplayRole ) const;
     Q_INVOKABLE RTM::Task *taskForRow(const int row) const;
 
     friend class Service;
-protected:
+
     void addTask(Task *task);
     void clear();
-
 signals:
-    
+
 public slots:
-    
+
 private:
     class Private;
     Private * const d;
